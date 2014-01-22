80f62ec503223574e86173f3a8c7b65bfc14df0d
Lee Avital <leeavital@gmail.com>
2014-01-22 16:46:39 -0500

    watchable tasklist

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
 'use strict'
 
-require( ['js/angular/angular.min.js' ], function(){
+require( ['js/angular/angular.js', 'js/watch/src/watch.js' ], function( ang, WatchJS){
 
 
-    var app = angular.module( 'daily-todo', [] );
+   var app = angular.module( 'daily-todo', [] );
 
-    app.controller( 'dt.MainCtrl', [ '$scope', 'dt.tasks', function( $scope, tasks  ){
-        $scope.message = 'Hello World';
-
-        $scope.addTask = function(){
-            console.log( $scope.taskform );
-            tasks.addTask( taskform );
-
-        }
-    }]);
-    
-
-    app.service( 'dt.tasks', [ function(){
-        
-        var allTasks = [];
-        
-        return {
-            addTask: function( task ){
-               allTasks.push( task );                    
-            }
-        }
+   app.controller( 'dt.MainCtrl', [ '$scope', 'dt.tasks', function( $scope, tasks  ){
 
 
-    } ]);
+      $scope.addTask = function(){
+         tasks.addTask( $scope.taskform );
+         $scope.$apply;
+      }
+
+      $scope.todaysTasks = tasks.todaysTasks;
+
+      
+
+   }]);
+
+
+   app.service( 'dt.tasks', [  function( ){
+var allTasks = []; 
+      var o = {};
+
+      o.addTask =  function( task ){
+         allTasks.push( task );        
+
+         o.todaysTasks.push(  );
+      }
+         o.todaysTasks.push( Object.create( task ) );
+      o.todaysTasks = [];
+
+      return o;
+
+
+
+   } ]);
 
 
 
